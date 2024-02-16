129ff37caf91ed2288ae7c88526e5dfdb0acf6a8
Andyeeeee <10514f074@gmail.com>
2024-02-16 14:04:31 +0800

    ewrewr

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-import { compoment } from "./js/compoment.js";
+export function compoment(x, y) {
+  fetch(x)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(y).innerHTML = data;
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Theres was an error!', error);
+    })
+}
 
-export { compoment }
+compoment('/footer.html', 'footer')
+compoment('/compoment/qq.html', 'qq')
