a01639e84d96abc79b47a424435efffd691885fe
Andyeeeee <10514f074@gmail.com>
2024-02-16 15:07:45 +0800

    asdasd

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-export function compoment(x, y) {
-  fetch(x)
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById(y).innerHTML = data;
-    })
-    .catch(error => {
-      console.error('Theres was an error!', error);
-    })
-}
-
-compoment('footer.html', 'footer')
+import { compoment } from "./compoment.js";
+export { compoment }
